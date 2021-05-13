24f894bee286ae79ec01d21f1e5d444ef2abece1
Evan Jeffries <ejeffries14@gmail.com>
2021-05-13 02:14:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |name|
+   "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+  array.collect do |item|
+    item.each_with_index do |i, r|
+      i + 1 r
+    end
+  end
+end
