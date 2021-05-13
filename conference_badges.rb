26adfdde7aa46ba182657323380f67299344c139
Evan Jeffries <ejeffries14@gmail.com>
2021-05-13 02:43:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,7 @@ def printer(array)
    batch_badge_creator(array).each do |list|
      puts list
    end
-  assign_rooms(array).each do ||
+  assign_rooms(array).each do |list|
+    puts list
   end
 end
