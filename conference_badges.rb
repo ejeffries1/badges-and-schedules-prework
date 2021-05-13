a7955a02e523607336f1ac812cc454f559c5e9cb
Evan Jeffries <ejeffries14@gmail.com>
2021-05-13 02:21:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(array)
   room = 0
   array.collect do |name|
-    "Hello, #{name}! You'll be assigned to room #{room}!"
     room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
